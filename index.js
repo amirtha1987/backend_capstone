adb7209daa2db49e8f0824cc51bda8587b4a9f34
amirthavalli1987 <amirtha.dit@gmail.com>
2024-11-13 19:54:56 +0530

    third commit

--- index.js
+++ index.js
@@ -6,13 +6,25 @@ const userRouter = require('./Routes/auth')
 const cookieParser = require("cookie-parser");
 const petRouter = require('./Routes/petr')
 
+
+const PORT = process.env.PORT || 3001
 const app = express()
 
 app.use(cors({
+
     origin: ["http://localhost:5173"],
     methods: ["GET", "POST","PUT"],
     credentials: true
 }))
+
+
+app.use('/', (req, res) => {
+    res.json({
+        status: "ApI working fine",
+        code:200
+    })
+
+})
 app.use(cookieParser())
 app.use(express.json());
 app.use('/auth', userRouter)
@@ -20,6 +32,10 @@ app.use('/pet', petRouter)
 
 
 mongoose.connect('mongodb://0.0.0.0:27017/user')
+
+
+
+
 
 
 
